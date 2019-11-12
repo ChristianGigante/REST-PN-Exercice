b4e9a1ad0cfc780ef6ff83495c876b314e018a78
ChristianGigante <christian.gigante@student.passerellesnumeriques.org>
2019-11-12 08:58:26 +0800

    updated

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3436
-const bodyParser= require('body-parser')
+const bodyParser = require('body-parser')
 
 //
 // Requirement for set up the exercise
 //
 app.use(bodyParser.json()); // parse requests of content-type - application/json
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-     next();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    next();
 });
 
 //
@@ -24,23 +24,51 @@ app.use(function(req, res, next) {
 // ------------------------------
 
 let recipes = [
-    { id:0, name: 'Spaghetti Bolognese', ingredients:["onion", "spaghetti", "beef", "tomato sauce"], purchasePrice:30, sellingPrice:50},
-    { id:1, name: 'Chicken Burger', ingredients:["onion", "tomato", "chicken", "bread", "creamy sauce", "cheese"], purchasePrice:50, sellingPrice:100},
-    { id:2, name: 'Chicken curry with rice', ingredients:["rice", "chicken", "salt", "curry pasta"], purchasePrice:45, sellingPrince:70},
-    { id:3, name: 'Pizza with peppers', ingredients:["pasta","onion", "peppers", "ham", "tomato sauce", "cheese"], purchasePrice:80, sellingPrice:110}
+    { id: 0, name: 'Spaghetti Bolognese', ingredients: ["onion", "spaghetti", "beef", "tomato sauce"], purchasePrice: 30, sellingPrice: 50 },
+    { id: 1, name: 'Chicken Burger', ingredients: ["onion", "tomato", "chicken", "bread", "creamy sauce", "cheese"], purchasePrice: 50, sellingPrice: 100 },
+    { id: 2, name: 'Chicken curry with rice', ingredients: ["rice", "chicken", "salt", "curry pasta"], purchasePrice: 45, sellingPrince: 70 },
+    { id: 3, name: 'Pizza with peppers', ingredients: ["pasta", "onion", "peppers", "ham", "tomato sauce", "cheese"], purchasePrice: 80, sellingPrice: 110 }
 ]
 
 // Question 1 : As a manager you want to fetch all the recipe. 
 // Create a HTTP Request :
+app.get("/recipes", (req, res) => {
+    res.send(recipes)
+})
 
 
 // Question 2 : As a manager you want to get only one recipe depends on his id.
 // Create a HTTP Request :
+app.get("/recipes/:id", (req, res) => {
+    const recipe = recipes.find(recipe => recipe.id === req.params.id);
+    //query database
+    if (!recipe) res.status(404).send('Recipe does not exist');
+    res.send(recipe);
 
+    //findMatchRecipe
+    // const recipesTofind = [];
+    // recipes.forEach(recipe => {
+    //     if (recipe.name === req.params.name) {
+    //         recipesTofind.push(recipe)
+    //     }
+    // })
+    // if (recipesTofind.length === 0) res.status(500).send('No Recipe exist');
+    // res.status(200).send(recipesTofind)
+})
 
 // Question 3 : As a manager you want to modify the selling price of only one recipe.
 // Create a HTTP Request :
+app.put("/recipes/:id", (req, res) => {
+    // console.log(">>>"+req.body.sellingPrice)
 
+    const index = recipes.findIndex(recipe => recipe.id == req.params.id);
+    console.log(index)
+    if (index == -1) {
+        res.status(404).send('Recipe to update does not exist!')
+    }
+    recipes[index].sellingPrice = req.body.sellingPrice;
+    res.send(recipes);
+})
 
 // Question 4 : As a manager you want to delete one recipe from the recipes list
 // Create a HTTP Request :
